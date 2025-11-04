a31fceb9889e5f7923ea5ee950b03f75001ec522
meek2100 <dtheurer@live.com>
2025-11-04 10:18:53 -0800

    Fix test failure.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -527,7 +527,7 @@ def test_config_override_precedence(tmp_path):
     )
 
     expected_content = (
-        "A very long line to test wrapping and EOLs.\r\nA very very long\r\nline.\r\n"
+        "A very long line to test wrapping and EOLs.\r\nA very very long line.\r\n"
     )
 
     assert run([str(file_path), "--config", str(explicit_config_path)]) == 0
